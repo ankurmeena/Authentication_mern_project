f2d6dc8c418a1040e983f854d44c2d667163e98f
ankurmeena <ankurmeena355@gmail.com>
2024-03-27 20:59:55 +0530

    mongodb setup

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-app.listen(3000,()=>{
-    console.log('server listening on port 3000 !');
+
+mongoose.connect(
+ 'mongodb+srv://ankurmeena355:gJPvxTn9drLvPTfS@ankur.ulpu3.mongodb.net/auth-mern?retryWrites=true&w=majority&appName=ankur'
+).then(()=>{
+    console.log('connected to MongoDB');
+})
+
+// Define a simple route handler for GET request
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
+app.listen(3000, () => {
+  console.log("server listening on port 3000 !");
 });
