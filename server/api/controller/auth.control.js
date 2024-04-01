29528804b28eb705d74b68da01922302728791ca
ankurmeena <ankurmeena355@gmail.com>
2024-04-02 00:30:54 +0530

    signin backend

--- server/api/controller/auth.control.js
+++ server/api/controller/auth.control.js
@@ -1,19 +1,45 @@
 import User from "../models/users.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
 import { errorhandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  if (typeof password !== 'string' || password.length === 0) {
-    throw new Error('Invalid password');
+  if (typeof password !== "string" || password.length === 0) {
+    throw new Error("Invalid password");
   }
   const salt = bcryptjs.genSaltSync(10);
-  const hashedPassword = bcryptjs.hashSync(password,salt);
-  const newUser = new User({ username, email, password:hashedPassword});
+  const hashedPassword = bcryptjs.hashSync(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(errorhandler(500,'something getting wrong with the credentials'));
+    next(errorhandler(500, "something getting wrong with the credentials"));
     // next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorhandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorhandler(401, "Invalid credentials "));
+    }
+
+    const expiryDate = new Date(Date.now()+3600000);
+
+    const { password: hashpassword, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("success_token", token, { httpOnly: true })
+      .status(200)
+      .json({...rest,totalexpiration: expiryDate});
+  } catch (error) {
+    next(error);
   }
 };
